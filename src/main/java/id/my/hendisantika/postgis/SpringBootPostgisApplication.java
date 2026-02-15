708d0b0ea4ca7a0660a7e2abd19d320e8c55578c
hendisantika <hendisantika@yahoo.co.id>
2026-02-15 08:35:38 +0700

    feat: init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/postgis/SpringBootPostgisApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgisApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/postgis/SpringBootPostgisApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
